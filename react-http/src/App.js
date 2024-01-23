6e3a42f5f9649739a8209aedb9620e75c0900f3c
CPI-RDRAMOS <ramosrichard310@gmail.com>
2024-01-23 09:16:19 +0800

    full

---
+++ react-http/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import GetList from './components/GetList';
+import PostForm from './components/PostForm';
+import PostList from './components/PostList';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <PostList /> */}
+      {/* <PostForm /> */}
+      <GetList />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-http/src/components/GetList.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class GetList extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         posts: [],
+         errorMsg: ''
+      }
+    }
+
+    clickHandler = () => {
+        axios.get('https://jsonplaceholder.typicode.com/posasdts')
+        .then((response) => {
+            this.setState({
+                posts: response.data
+            })
+        })
+        .catch((error) => {
+            this.setState({
+                errorMsg: 'Unavaible to load data'
+            })
+        })
+    }
+    
+    render() {
+        const {posts, errorMsg} = this.state
+        return (
+        <div>
+            JSON Fake API Data List
+            <button onClick={this.clickHandler}>Generate List</button>
+            {
+                posts.length ?
+                posts.map((post) => <div key={post.id}>{post.title}</div>) : null
+            }
+            {
+                errorMsg ? <div>{errorMsg}</div> : ''
+            }
+        </div>
+        )
+    }
+}
+
+export default GetList
